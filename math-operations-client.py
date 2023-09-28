1b8870ae915b122a514b3038ff24f55c6508c283
Tana <thliu@smith.edu>
2023-09-27 23:39:26 -0400

    9/27

--- math-operations-client.py
+++ math-operations-client.py
@@ -2,17 +2,29 @@
 
 import socket
 
-HOST = "127.0.0.1"  # The server's hostname or IP address
-PORT = 65432  # The port used by the server
+ # the server's hostname or IP address
+HOST = "127.0.0.1" 
+# the port used by the server
+PORT = 65432  
+
+# asks client to input a number, operator, number to form the equation
 MSG = input("Enter the operation in the form 'number operator number': ")
 
 print("client starting - connecting to server at IP", HOST, "and port", PORT)
+
+# creates a socket object and uses it without needing to call .close()
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+
+    # connects to the server 
     s.connect((HOST, PORT))
     print(f"connection established, sending request '{MSG}'")
+
+    # encodes the client's inputs and sends data from bytes to the server until all data has been sent
     s.sendall(MSG.encode())
     
     print("request sent, waiting for reply")
+
+    # reads the server's reply
     data = s.recv(1024)
     
 
--- math-operations-server.py
+++ math-operations-server.py
@@ -2,21 +2,40 @@
 
 import socket
 
+# standard loopback interface address (localhost)
+HOST = "127.0.0.1"  
 
-HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
-PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
+# port to listen on (non-privileged ports are > 1023)
+PORT = 65432  
+
+# empty string msg to hold the decoded message
 msg = ""
 
 print("server starting - listening for connections at IP", HOST, "and port", PORT)
+
+# creates a socket object and uses it without needing to call .close()
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+
+    # associates the socket with a specific IP address and port number
     s.bind((HOST, PORT))
+
+    # listening socket to allow the server to accept connections 
     s.listen(1)
+
+    # accepts incoming connection request from client and returns a socket connected to the client
     conn, addr = s.accept()
+
     with conn:
         print(f"Connected established with {addr}")
         while True:
+
+            #reads the data sent by the client and returns the length of the request in bytes
             data = conn.recv(1024)
+
+            #decodes the data sent by the client
             msg = data.decode()
+
+            #terminates loop if the data is an empty bytes object b''
             if not data:
                 break
             print(f"Client equation has been received: '{data!r}' [{len(data)} bytes]")
@@ -37,8 +56,10 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             elif operation == "*":
                 total = num1 * num2
 
+            #converts the total from int to str 
             answer = str(total)
+
+            #sends the encoded result to the client and sends data from bytes until all data has been sent
             conn.sendall(answer.encode())
-    conn.close()
 
 print("server is done!")
